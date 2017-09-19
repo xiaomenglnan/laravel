56e32aa51572b38c97c13dcb507fc608a63d6ec1
menglingnan <menglingnan@guazi.com>
2017-09-19 02:04:10 +0000

    modeling-users 创建用户对象

---
+++ .php
@@ -0,0 +1,71 @@
+{
+"status": 200,
+"msg": "",
+"data": [
+{
+"id": "pick_seeds_c1_workbench",
+"title": "检瓜子C1工作台",
+"export": "http://api.bi.guazi-corp.com/api/dashboard?aggregate=&compare_end_date=&compare_start_date=&compare_yesterday=&end_date=2017-08-31&ids=pick_seeds_c1_workbench&query_city=&start_date=2017-07-01&export=pick_seeds_c1_workbench",
+"help": "",
+"table": [
+{
+"time": "时间",
+"aggregate": "分组",
+"finance_set_c2c_and_consigned": {
+"title": "财务已定",
+"describe": null,
+"indicator_type": "indicator",
+"indicator": "finance_set_c2c_and_consigned",
+"time_aggregate": 7
+}
+},
+{
+"time": "17/07/01-17/08/31",
+"aggregate": "汇总",
+"finance_set_c2c_and_consigned": 1736
+},
+{
+"time": "17/07/01-17/08/31",
+"aggregate": {
+"aggregate": "org_area",
+"aggregate_value": "eNqtkEFqAjEUhj3LO0EmraN9WQlScKEjiF3NJkNCjEzNkEQQRPAAuhN6gm57gG56moH2Fn3TqWJ3hXaX%2FP%2Bf7%2F15GXbvEAZVlQ%2BM8drIqPPpZqYr6Zvj0D7qVbBulY%2Bd0mXIM28u2pcE2MdtQIJE92B9XMsSRIFMBEwSBNWQIj2IAYREjlvbel2E9%2Be3%2BvD0cdrXp5f68ArCIr%2ByjtfWjvQbwp1n35fSgJhQPKXRnd9%2F4Gf7jpJhUTjp1Ui1uB77F1xWLL%2Fr8b%2Fw1kH7lpMQp9RSaT8nrWlLbATOerdp2lyS9ByYumAjIdoQbXPIGOO0w095mLQO",
+"aggregate_title": "西区"
+},
+"finance_set_c2c_and_consigned": 292
+},
+{
+"time": "17/07/01-17/08/31",
+"aggregate": {
+"aggregate": "org_area",
+"aggregate_value": "eNqtkMFKAzEQhvsseYLMals7ORWK4EG3IHraS5aEGFmbJUlBKIIPsHoq9OIjiNCb9Hm6B9%2FC2W5XrCdBb8n%2F%2F%2FPxz6TYHyEbl2U2NsZrI6POpveXupS%2BeU7snZ4F62bZuVO6CFnqzZe2kxie4CIgQaK7tj7OZcFEjlwEBECmGlKkgRiYkHiMC9t6fWT103NdrT6Wj%2FXyra42TFhMOmu93r6%2FHFijvUX6zymAbqxabTev37wH0o%2BoRVf5tJCGiQuKD6hx7%2Fd7Hy7dUzLc5E56daZa3JD%2FCy7Nb%2Ff1kr%2Fw5kH7lgPEKbRU2l%2BR1rQlNjKABIbQfGDQBaYu2EiINkTXnHDOE7rhJ%2BivzwE%3D",
+"aggregate_title": "北区"
+},
+"finance_set_c2c_and_consigned": 591
+},
+{
+"time": "17/07/01-17/08/31",
+"aggregate": {
+"aggregate": "org_area",
+"aggregate_value": "eNrztzK1tFJyLCiIcUxPL0pNTyxJjQmoCE4tSCwCMV0yc1PzijPz82J881NSc4pj%2FIvS4WJgISUrC6vqYiugISX5YZlFJaWJOUrWSVYG1sVWhoZWSikgk0qAGkqKlawTrYytqjMhcqZWSi%2B6%2B5%2FPmvZySsPTKeue9uxSss60MoZKPds689nG%2BShShjC5Jzu6nm3djiRXCxQ3BloFc5dbTmK6krUfULkZ0FkMxHsO1WcMKYnFGUn5iUUpnikQ48wNqGKcf1IW1HlGlJhXWpxaBDHHEGhOTmpiSmpRKFAM5NpiKxMrJRMLAxMQ09AMJh2QX5xZAjQAogQYli4GBgZGwBAEAFsCwVQ%3D",
+"aggregate_title": "东区"
+},
+"finance_set_c2c_and_consigned": 460
+},
+{
+"time": "17/07/01-17/08/31",
+"aggregate": {
+"aggregate": "org_area",
+"aggregate_value": "eNrztzK1tFJyLCiIcUxPL0pNTyxJjQmoCE4tSCwCMV0yc1PzijPz82J881NSc4pj%2FIvS4WJgISUrC6vqYiugISX5YZlFJaWJOUrWSVYG1sVWhoZWSikgk0qAGkqKlawTrYytqjMhchZWSs92znixcfXLlqaXUxqeTln3tGeXknWmlSFI1tRK6cmOJU937keRMoZKvZy%2B98XWxUhStUBxY6BlMJe55SSmK1n7AZWbAR3GQLz3UP3GkJJYnJGUn1iU4pkCMc7cgCrG%2BSdlQZ1nRIl5pcWpRRBzDIHm5KQmpqQWhQLFQK4Fmm2lZGhoYmJkCuIYmsEUBOQXZ5YAjYAoAoami4GBgREwDAG6%2BcQQ",
+"aggregate_title": "南区"
+},
+"finance_set_c2c_and_consigned": 393
+}
+],
+"charts": [
+"table"
+],
+"description": "检瓜子C1工作台",
+"pieChart": []
+}
+]
+}
